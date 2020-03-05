49f33b3deca2706bb3411262e5f457d57a8d623a
Harsha <ashharsha1999@gmail.com>
2020-03-05 15:03:12 +0530

    tested other classification methods

--- kmeans.py
+++ kmeans.py
@@ -3,6 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
 
 le_s = LabelEncoder()
 le_i = LabelEncoder()
@@ -20,10 +27,29 @@ df['acq_date'] = le_acd.fit_transform(df['acq_date'])
 y = df.confidence.values
 x = df.drop('confidence', axis = 1).reset_index(drop = True)
 
-km = KMeans(n_clusters = 3, init = 'k-means++', max_iter = 100, verbose = 0)
+km = KMeans(n_clusters = 3, init = 'k-means++', max_iter = 100, verbose = 1)
 
 x_train,x_val, y_train, y_val = train_test_split(x,y, test_size=0.3)
 
 km.fit(x_train)
 predictions = km.predict(x_val)
-print(predictions)
+
+# Calculate Accuracy and other metrics
+print("Classification Metrics:")
+accuracyv = accuracy_score(predictions, y_val)
+print('Accuracy: %f' % accuracyv)
+precision = precision_score(predictions, y_val, average = 'macro')
+print('Precision: %f' % precision)
+recall = recall_score(predictions, y_val, average = 'macro')
+print('Recall: %f' % recall)
+f1 = f1_score(predictions, y_val, average = 'macro')
+print('F1 score: %f' % f1)
+
+# Plot Confusion Matrix
+labels = ['h','l','n']
+cm = confusion_matrix(y_val, predictions)
+df_cm = pd.DataFrame(cm, index = labels,columns = labels)
+fig = plt.figure()
+sns.heatmap(df_cm, annot=True)
+fig.tight_layout()
+plt.savefig('kmeans.png')
--- knn.py
+++ knn.py
@@ -1,15 +1,16 @@
-import pandas as pd
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
+from sklearn.neighbors import KNeighborsClassifier
 import joblib
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+
 
 # Data Preprocessing
 le_s = LabelEncoder()
@@ -33,37 +34,25 @@ x = df.drop('confidence', axis = 1).reset_index(drop = True)
 print('Spliting the data into train and validation sets...')
 x_train,x_val, y_train, y_val = train_test_split(x,y, test_size=0.3)
 
-# Train the SVM
-print('SVM Training..')
-svm_model_linear = SVC(kernel = 'linear', C = 1, verbose = True).fit(x_train, y_train)
-svm_predictions = svm_model_linear.predict(x_val)
-
-filename = 'SVM_model.sav'
-# print('Saving the model....')
-# joblib.dump(svm_model_linear, filename)
-# print(filename + ' Saved....')
-
-## Load the  model and make predictions
-print('Loading the model...')
-svm_model_linear = joblib.load(filename)
-svm_predictions = svm_model_linear.predict(x_val)
+# Train the KNN Algorithm
+knn = KNeighborsClassifier(n_neighbors = 7).fit(x_train, y_train)
+predictions = knn.predict(x_val)
 
 # Calculate Accuracy and other metrics
 print("Classification Metrics:")
-accuracyv = accuracy_score(svm_predictions, y_val)
+accuracyv = accuracy_score(predictions, y_val)
 print('Accuracy: %f' % accuracyv)
-precision = precision_score(svm_predictions, y_val, average = 'macro')
+precision = precision_score(predictions, y_val, average = 'macro')
 print('Precision: %f' % precision)
-recall = recall_score(svm_predictions, y_val, average = 'macro')
+recall = recall_score(predictions, y_val, average = 'macro')
 print('Recall: %f' % recall)
-f1 = f1_score(svm_predictions, y_val, average = 'macro')
+f1 = f1_score(predictions, y_val, average = 'macro')
 print('F1 score: %f' % f1)
 
-# Plot Confusion Matrix
 labels = ['h','l','n']
-cm = confusion_matrix(y_val, svm_predictions)
+cm = confusion_matrix(y_val, predictions)
 df_cm = pd.DataFrame(cm, index = labels,columns = labels)
 fig = plt.figure()
 sns.heatmap(df_cm, annot=True)
 fig.tight_layout()
-plt.savefig('cm_svm.png')
+plt.savefig('cm_knn.png')
